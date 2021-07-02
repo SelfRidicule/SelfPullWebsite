34feaa69ece79de97d17c88335e56a29e3ac2ab6
SelfRidicule <selfridicule@gmail.com>
2021-07-02 22:41:53 +0800

    描述：冒烟测试

--- src/main/java/com/github/selfridicule/Main.java
+++ src/main/java/com/github/selfridicule/Main.java
@@ -1,10 +1,35 @@
 package com.github.selfridicule;
-import java.util.*;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    public static void accessNetwork() throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://www.baidu.com");
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+        try {
+            System.out.println(response.getStatusLine());
+            HttpEntity entity = response.getEntity();
+            InputStream is = entity.getContent();
+            //is to string
+            String content = IOUtils.toString(is, "UTF-8");
+            System.out.println(content);
+        } catch (Exception e) {
+            response.close();
+        }
     }
 
 }
---
+++ src/test/java/com/github/selfridicule/SmokeTest.java
@@ -0,0 +1,10 @@
+package com.github.selfridicule;
+
+import org.junit.jupiter.api.Test;
+
+public class SmokeTest {
+    @Test
+    public void test(){
+
+    }
+}
